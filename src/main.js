5fb4c18e45b577c95438cc353eeeb5fe8ed6034e
Rayvi-Ahmed <rayvi73@gmail.com>
2024-02-08 11:41:52 +0600

    menu item & review section has been added

--- src/main.js
+++ src/main.js
@@ -15,6 +15,10 @@ closeMenu.addEventListener('click',()=>{
 
 // Tab//
 const tabs= document.querySelectorAll('.tebs_Item ul li')
+const all = document.querySelectorAll('all')
+const food = document.querySelectorAll('food')
+const snack = document.querySelectorAll('snack')
+const bevarage = document.querySelectorAll('bevarage')
 
 tabs.forEach(tab=>{
     tab.addEventListener('click',()=>{
@@ -24,7 +28,36 @@ tabs.forEach(tab=>{
     })
 
     tab.classList.add('active')
-      
+
+    all.forEach(item=>{
+        item.style.display='none'
+    })
+
+    const tabval =tab.getAttribute('data-tabs')
+    
+
+    if(tabval=== "food"){
+        food.forEach(item=>{
+            item.style.display="block"
+        })
+        
+    }else if(tabval==='snack'){
+        snack.forEach(item=>{
+            item.style.display="block"
+        })
+
+        }
+        else if(tabval=== 'bevarage'){
+            bevarage.forEach(item=>{
+                item.style.display='block'
+            })
+
+    }else{
+        all.forEach(item=>{
+            item.style.display='block'
+        })
+    }
+    
     })
 
 })
